e9b0a2ce9f1972697e3ecab1e332ce954ec9c73c
tackyunicorn <joshjoseph97@gmail.com>
2019-01-09 12:55:56 +0530

    added math

---
+++ math.rb
@@ -0,0 +1,24 @@
+puts -5.86543
+puts 5 + 9    # Addition
+puts 5 - 9    # Subtraction
+puts 5 * 9    # Multiplication
+puts 9 / 3    # Division
+puts 2 ** 3   # Exponentiation
+puts 10 % 3   # Modulus
+
+num = 7
+puts ("My favorite number is " + num.to_s) # Cast integer to string
+
+num = -3.4
+puts num.abs()   # Absolute value
+
+num = 5.67
+puts num.round() # Rounding off
+puts num.ceil()  # Ceil value
+puts num.floor() # Floor value
+
+puts Math.sqrt(36) # Square Root
+puts Math.log(1)   # Logarithm
+
+puts 10 / 7   # Integer Division
+puts 10.0 / 7 # Floating point division
