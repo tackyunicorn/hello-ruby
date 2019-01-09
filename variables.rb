7da8580c45f5a361b5f4bc457340fe9da7f1b0a9
tackyunicorn <joshjoseph97@gmail.com>
2019-01-09 12:09:38 +0530

    added variables

---
+++ variables.rb
@@ -0,0 +1,11 @@
+name = "Mike"
+age = "45"
+
+puts ("There was once a man named " + name)
+puts ("he was " + age + " years old")
+
+name = "Tom"
+age = "35"
+
+puts ("he really liked the name " + name)
+puts ("but didn't like being " + age)
