7ecb2ba0a5e5a8e5d6227106ced5921ae17dd5d1
tackyunicorn <joshjoseph97@gmail.com>
2019-01-10 20:12:19 +0530

    added input

---
+++ input.rb
@@ -0,0 +1,7 @@
+puts "Enter your name: "
+name = gets.chomp()                        # chomp removes the newline after entering the text
+puts ("Hello " + name + ", you are cool!")
+
+puts "Enter your age: "
+age = gets.chomp()
+puts ("Hello " + name + ", you are " + age + " years old")
